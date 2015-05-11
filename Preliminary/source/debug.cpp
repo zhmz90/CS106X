9167555aca68e8b61e1e4fcd8a19a3c92411a1c7
Lanting Guo <guolanting@haplox.com>
2015-05-11 20:09:33 +0800

    start C++ with Qt followed CS106X

---
+++ Preliminary/source/debug.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    Vector<int> n;
+    n.add(2);
+    n.add(3);
+    n.add(4);
+
+    cout<<"n is: "<<n<<endl;
+
+
+    return 0;
+}
